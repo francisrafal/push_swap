bf323775fffa1720097026d85c816c4fb4db0d6d
Francis Rafal <francis.rafal@gmail.com>
2023-01-13 18:39:40 +0100

    feat: print_stacks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 17:16:12 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/13 18:21:19 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/13 18:38:41 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,27 +63,37 @@ t_stack	*init_stack(void)
 	return (stack);
 }
 
+void	print_stacks(t_stack a, t_stack b)
+{
+	(void)b;
+
+	ft_printf("--------------------------------------------\n\n");
+	while (a.head)
+	{
+		ft_printf("%d\n", a.head->data);
+		a.head = a.head->next;
+	}
+	ft_printf("_ _\n");
+	ft_printf("a b\n\n");
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 	t_stack	*a;
+	t_stack	*b;
 
 	if (argc == 1)
 		exit(EXIT_FAILURE);
 	a = init_stack();
-	push(a, 10);
-	ft_printf("size: %u\n", a->size);
-	ft_printf("data: %d\n", a->head->data);
-	push(a, 20);
-	ft_printf("size: %u\n", a->size);
-	ft_printf("data: %d\n", a->head->data);
+	b = init_stack();
+	push(a, 1);
+	push(a, 2);
+	print_stacks(*a, *b);
 	pop(a);
-	ft_printf("size: %u\n", a->size);
-	ft_printf("data: %d\n", a->head->data);
-	push(a, -40);
-	ft_printf("size: %u\n", a->size);
-	ft_printf("data: %d\n", a->head->data);
+	push(a, -4);
+	print_stacks(*a, *b);
 	free_stack(a);
 	return (0);
 }
