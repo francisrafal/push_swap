8b77d88087299dd4f7306bacb9d6b9da176068bd
Francis Rafal <francis.rafal@gmail.com>
2023-01-17 15:28:46 +0100

    feat: Add rotate

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 17:16:12 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/17 14:03:50 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/17 15:27:53 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	pa(t_data *data)
 {
 	push(data->a, pop(data->b), data);
-	ft_printf("pa\n");
+	ft_printf("%s\n", __func__);
 }
 
 void	pb(t_data *data)
 {
 	push(data->b, pop(data->a), data);
-	ft_printf("pb\n");
+	ft_printf("%s\n", __func__);
 }
 
 void	sa(t_data *data)
@@ -61,6 +61,100 @@ void	ss(t_data *data)
 	data->called_directly = CALLED_DIRECTLY;
 }
 
+void	ra(t_data *data)
+{
+	t_node	*tmp;
+	t_node	*runner;
+
+	if (data->a->size <= 1)
+		return ;
+	tmp = data->a->head;
+	runner = data->a->head;
+	data->a->head = data->a->head->next;
+	while (runner->next)
+		runner = runner->next;
+	runner->next = tmp;
+	tmp->next = NULL;
+	if (data->called_directly == CALLED_DIRECTLY)
+		ft_printf("%s\n", __func__);
+}
+
+void	rb(t_data *data)
+{
+	t_node	*tmp;
+	t_node	*runner;
+
+	if (data->b->size <= 1)
+		return ;
+	tmp = data->b->head;
+	runner = data->b->head;
+	data->b->head = data->b->head->next;
+	while (runner->next)
+		runner = runner->next;
+	runner->next = tmp;
+	tmp->next = NULL;
+	if (data->called_directly == CALLED_DIRECTLY)
+		ft_printf("%s\n", __func__);
+}
+
+void	rr(t_data *data)
+{
+	data->called_directly = CALLED_WRAPPED;
+	ra(data);
+	rb(data);
+	ft_printf("%s\n", __func__);
+	data->called_directly = CALLED_DIRECTLY;
+}
+
+void	rra(t_data *data)
+{
+	t_node	*last;
+	t_node	*second_to_last;
+
+	if (data->a->size <= 1)
+		return ;
+	second_to_last = data->a->head;
+	while (second_to_last->next->next)
+		second_to_last = second_to_last->next;
+	last = data->a->head;
+	while (last->next)
+		last = last->next;
+	last->next = data->a->head;
+	data->a->head = last;
+	second_to_last->next = NULL;
+	if (data->called_directly == CALLED_DIRECTLY)
+		ft_printf("%s\n", __func__);
+}
+
+void	rrb(t_data *data)
+{
+	t_node	*last;
+	t_node	*second_to_last;
+
+	if (data->b->size <= 1)
+		return ;
+	second_to_last = data->b->head;
+	while (second_to_last->next->next)
+		second_to_last = second_to_last->next;
+	last = data->b->head;
+	while (last->next)
+		last = last->next;
+	last->next = data->b->head;
+	data->b->head = last;
+	second_to_last->next = NULL;
+	if (data->called_directly == CALLED_DIRECTLY)
+		ft_printf("%s\n", __func__);
+}
+
+void	rrr(t_data *data)
+{
+	data->called_directly = CALLED_WRAPPED;
+	rra(data);
+	rrb(data);
+	ft_printf("%s\n", __func__);
+	data->called_directly = CALLED_DIRECTLY;
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*data;
@@ -84,6 +178,18 @@ int	main(int argc, char **argv)
 	sb(data);	
 	print_stacks(*(data->a), *(data->b));
 	ss(data);	
+	print_stacks(*(data->a), *(data->b));
+	ra(data);	
+	print_stacks(*(data->a), *(data->b));
+	rb(data);	
+	print_stacks(*(data->a), *(data->b));
+	rr(data);	
+	print_stacks(*(data->a), *(data->b));
+	rra(data);	
+	print_stacks(*(data->a), *(data->b));
+	rrb(data);	
+	print_stacks(*(data->a), *(data->b));
+	rrr(data);	
 	print_stacks(*(data->a), *(data->b));
 	free_data(data);
 	return (0);
